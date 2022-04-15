d8b6e05540d5d5191afe7223d516c81a487cacbd
AnYiFan <921645699@qq.com>
2022-04-15 16:26:27 +0800

    cli筛选

--- clifile/cli.c
+++ clifile/cli.c
@@ -2,15 +2,89 @@
 #include<unistd.h>
 #include<string.h>
 #include<stdlib.h>
-#include<assert.h>
 #include<sys/socket.h>
 #include<arpa/inet.h>
 #include<netinet/in.h>
 
+int socket_create();
+char* get_cmd(char buff[],char* myargv[])
+{
+    if(buff == NULL || myargv == NULL)
+        return NULL;
+
+    int index = 0;
+    char *s = strtok(buff," ");
+    while( s != NULL)
+    {
+        myargv[index++] = s;
+        s = strtok(NULL," ");
+    }
+
+    return myargv[0];
+}
+
 int main()
 {
+    int sockfd = socket_create();
+    if(sockfd == -1)
+        exit(0);
+    printf("connect >>\n");
+    fflush(stdout);
+    while(1)
+    {
+
+        printf("input\n");
+        char buff[128]={0};
+        fgets(buff,127,stdin);
+        buff[strlen(buff)-1] = 0;
+        
+        char send_buff[128] = {0};
+        strcpy(send_buff,buff);
+        
+        char* myargv[10] = {0};
+        char cmd = get_cmd(buff,myargv);
+
+        if( cmd == NULL)
+            continue;
+
+        else if(strcmp(cmd,"exit") == 0)
+        {   
+            printf("ser close");
+            break;
+        }
+        else if( strcmp(cmd,"get") == 0)
+        {
+        }
+        else if( strcmp(cmd,"up") == 0)
+        {
+        }
+        else
+        {        
+            send(sockfd,send_buff,strlen(buff),0);
+            memset(send_buff,0,128);
+            if( recv(sockfd,send_buff,127,0)<=0)
+            {
+                printf("ser close\n");
+                break;
+            }
+            if( strncmp(send_buff,"ok#",3) == 0)
+            {
+                printf("%s\n",send_buff+3);
+            }
+            else
+            {
+                printf("服务器错误:%s\n",send_buff);
+            }
+        }
+    }
+    close(sockfd);
+    return 0;
+}
+int socket_create()
+{
     int sockfd=socket(AF_INET,SOCK_STREAM,0);
-    assert(sockfd!=-1);
+    if(sockfd == -1)
+        return -1;
 
     struct sockaddr_in saddr;
     memset(&saddr,0,sizeof(saddr));
@@ -19,23 +93,11 @@ int main()
     saddr.sin_addr.s_addr=inet_addr("127.0.0.1");
 
     int res=connect(sockfd,(struct sockaddr*)&saddr,sizeof(saddr));
-    assert(res!=-1);
-    
-    while(1)
+    if(res == -1)
     {
-        printf("input\n");
-        char buff[128]={0};
-        fgets(buff,127,stdin);
-        
-        if(strncmp(buff,"exit",4) == 0)
-        {   
-            printf("ser close");
-            break;
-        }
-        send(sockfd,buff,strlen(buff),0);
-        memset(buff,0,128);
-        recv(sockfd,buff,127,0);
-        printf("buff=%s\n",buff);
+        printf("connect err\n");
+        close(sockfd);
+        return -1;
     }
-    close(sockfd);
+    return sockfd;
 }
